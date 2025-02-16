415a24cd3721251d7fe0eb87460181b0d48b5e68
morikazu1119 <morikawakazuki11.19@gmail.com>
2025-02-16 23:42:39 +0900

    add dfs

---
+++ tests/test_dfs.py
@@ -0,0 +1,13 @@
+from scripts.algorithm.dfs_func import dfs
+
+def test_dfs():
+    graph = {
+        'A': {'B', 'C'},
+        'B': {'A', 'D', 'E'},
+        'C': {'A', 'F'},
+        'D': {'B'},
+        'E': {'B', 'F'},
+        'F': {'C', 'E'}
+    }
+    result = dfs(graph, 'A')
+    assert result == ['A', 'B', 'D', 'E', 'F', 'C']
